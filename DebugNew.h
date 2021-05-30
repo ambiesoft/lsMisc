9af16efb3264e99c857459e63b940a01b76879d5
ambiesoft <ambiesoft.trueff@gmail.com>
2021-05-31 03:07:27 +0900

    define defined

--- DebugNew.h
+++ DebugNew.h
@@ -3,6 +3,7 @@
 #ifdef _DEBUG
 #ifndef DEBUG_NEW_DEFINED
 #include <crtdbg.h>
+#define DEBUG_NEW_DEFINED
 #define malloc(size) _malloc_dbg(size, _NORMAL_BLOCK, __FILE__, __LINE__ )
 #define calloc(s1,s2) _calloc_dbg(s1, s2, _NORMAL_BLOCK, __FILE__, __LINE__ )
 #define realloc(p,s) _realloc_dbg(p, s, _NORMAL_BLOCK, __FILE__, __LINE__)
