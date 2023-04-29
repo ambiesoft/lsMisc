5db03f1b7de46ab6e5cdc1726796b844712a9aac
ambiesoft <ambiesoft.trueff@gmail.com>
2023-04-29 21:23:50 +0900

    end of stringEndWith

--- stringEndwith.h
+++ stringEndwith.h
@@ -21,5 +21,6 @@
 //OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 //SUCH DAMAGE.
 
-BOOL stringEndwith(LPCTSTR pString, LPCTSTR pEnd);
-BOOL stringEndwithI(LPCTSTR pString, LPCTSTR pEnd);
+// use stdEndWith in stdosd.h
+//BOOL stringEndwith(LPCTSTR pString, LPCTSTR pEnd);
+//BOOL stringEndwithI(LPCTSTR pString, LPCTSTR pEnd);
