7dcbbb494b75a0f6af71c7d1215d97b3471dfc1f
ambiesoft <ambiesoft.trueff@gmail.com>
2024-11-21 12:45:25 +0900

    Added a project 'guisamples'
    To test gui functions

---
+++ lsMiscTest/guisamples/guisamples.h
@@ -0,0 +1,32 @@
+
+// guisamples.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CguisamplesApp:
+// See guisamples.cpp for the implementation of this class
+//
+
+class CguisamplesApp : public CWinApp
+{
+public:
+	CguisamplesApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CguisamplesApp theApp;
